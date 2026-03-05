64c5307195451a5020c5c98bb72809ccb99edcaf
Henok Dereje <henzdereje@gmail.com>
2026-03-05 01:17:24 -0800

    i created separeted jsx file for core concepts in the components folder and added all the things included in the core concept

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,6 @@
 import { CORE_CONCEPTS } from "./data";
 import Header from "./components/Header";
-
-
-function CoreConcept({image, title, description}){
-  return(
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-}
+import CoreConcept from "./components/CoreConcepts";
 
 function App() {
   return (
@@ -30,5 +20,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
