31f0d3658c32316be81c45215c5122aa07b382bb
Henok Dereje <henzdereje@gmail.com>
2026-03-10 07:06:10 -0700

    the div wrapper in the App component is just unnecessary so i imported fragment from react and added it instead of the div

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, Fragment } from "react";
 
 import { CORE_CONCEPTS } from "./data";
 import Header from "./components/Header/Header";
@@ -24,7 +24,7 @@ function App() {
   }
 
   return (
-    <div>
+    <Fragment>
       <Header />
       <main>
         <section id="core-concepts">
@@ -62,7 +62,7 @@ function App() {
           {tabContent}
         </section>
       </main>
-    </div>
+    </Fragment>
   );
 }
 export default App;
